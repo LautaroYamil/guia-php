a7c1b5cc9d46336d3a21dd012df1c41a3714919d
LautaroYamil <lautaroyamil10@gmail.com>
2025-11-16 19:25:42 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Guía PHP - Integrador</title>
+</head>
+<body>
+    <h1>Guía PHP - Hasta el punto 8</h1>
+    <p>Seleccioná un ejercicio:</p>
+
+    <ul>
+        <li><a href="ejercicio4.php">Ejercicio 4 - Número Aleatorio</a></li>
+        <li><a href="ejercicio5.php">Ejercicio 5 - Tipos de Variables</a></li>
+        <li><a href="ejercicio6.php">Ejercicio 6 - Variables de tipo String</a></li>
+        <li><a href="ejercicio7.php">Ejercicio 7 - Condicionales</a></li>
+        <li><a href="ejercicio8.php">Ejercicio 8 - Estructuras Repetitivas</a></li>
+    </ul>
+</body>
+</html>
